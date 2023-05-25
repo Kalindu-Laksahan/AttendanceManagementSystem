2c93d4aec2d386b73b5d481b566622c275143b59
Kalindu Laksahan <68551659+Kalindu-Laksahan@users.noreply.github.com>
2023-05-25 13:55:04 +0530

    Add project files.

---
+++ AttendanceManagement/RJTextBox.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace Krypton_Toolkit_Demo
+{
+    partial class RJTextBox
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Dock = DockStyle.Fill;
+            textBox1.Location = new Point(10, 7);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(230, 20);
+            textBox1.TabIndex = 0;
+            // 
+            // RJTextBox
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            Controls.Add(textBox1);
+            Name = "RJTextBox";
+            Padding = new Padding(10, 7, 10, 7);
+            Size = new Size(250, 30);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+    }
+}
